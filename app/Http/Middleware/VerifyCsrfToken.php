4cfd47789e68d78f5a6d95c96b3b79077f33640a
Nguyen trung duc <ducnt.ts24@gmail.com>
2016-06-30 17:01:56 +0700

    Khoi tao project

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+
+class VerifyCsrfToken extends BaseVerifier
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+	
+	protected function tokensMatch($request){
+		if($request->wantsJon()){
+			return true;
+		}
+		return parten::tokensMatch($request);
+	}
+}
